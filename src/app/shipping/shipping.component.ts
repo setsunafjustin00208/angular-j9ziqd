690dd8e1e6462321f32bc8ab2df3afcd98781e34
setsunafjustin00208 <setsunafjustin00208@outlook.com>
2022-07-18 11:31:13 +0800

    1 file modified

--- src/app/shipping/shipping.component.ts
+++ src/app/shipping/shipping.component.ts
@@ -13,6 +13,7 @@ export class ShippingComponent implements OnInit {
   shippingCosts!: Observable<{ type: string, price: number }[]>;
 
   ngOnInit(): void {
+    this.shippingCosts =  this.cartService.getShippingPrices();
   }
 
 }
