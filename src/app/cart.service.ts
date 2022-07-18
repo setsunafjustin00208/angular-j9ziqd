0fc65ff7649433216aeb047f65af32db0f6f4f53
setsunafjustin00208 <setsunafjustin00208@outlook.com>
2022-07-18 10:52:06 +0800

    2 files modified, 4 added

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Product } from './products';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root',
@@ -18,5 +19,8 @@ export class CartService {
     this.items = [];
     return this.items;
   }
-  constructor() {}
+  getShippingPrices() {
+    return this.http.get<{type: string, price: number}[]>('/assets/shipping.json');
+  }
+  constructor(private http: HttpClient) {}
 }
